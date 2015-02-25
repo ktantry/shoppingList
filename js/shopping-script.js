b86c12208b7a54c70e7387219d8262af73ffa541
ktantry <kushaltantry@gmail.com>
2015-02-24 17:36:05 -0800

    Add and Delete Rows

--- js/shopping-script.js
+++ js/shopping-script.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
 	
 
 	$('.add-button').mousedown(function() {
-		/*$("#my-table tbody").app();*/
-		/*alert('Hello There');*/
 		$("#my-table tr:last").after("<tr>"+
-		"<td><input type='text'/></td>"+
+		"<td><input type='checkbox' class='case'/></td>"+
 		"<td><input type='text'/></td>"+
 		"<td><input type='text'/></td>"+
 		"<td><input type='text'/></td>"+
 		"<td><input type='text'/></td>"+
 		"</tr>");
 	});
+
+	$('.delete-button').mousedown(function() {
+		$('.case:checkbox:checked').parents("tr").remove();
+	});
+
 });
 
